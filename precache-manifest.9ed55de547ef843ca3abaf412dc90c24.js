0f912f9dbaa3631d72c2ab019fa36fa61e309f5c
Olegus <gulldan1996@gmail.com>
2020-03-29 04:44:18 +0300

    Updates

---
+++ precache-manifest.9ed55de547ef843ca3abaf412dc90c24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43a875a6b25cccc930bf45ec86637c9",
+    "url": "/durov_test/index.html"
+  },
+  {
+    "revision": "1e8982d3cdaaf2e60316",
+    "url": "/durov_test/static/css/main.f99d8a5e.chunk.css"
+  },
+  {
+    "revision": "46f3fc9ad9380b699b63",
+    "url": "/durov_test/static/js/2.5d1df464.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/durov_test/static/js/2.5d1df464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8982d3cdaaf2e60316",
+    "url": "/durov_test/static/js/main.a7adb6bf.chunk.js"
+  },
+  {
+    "revision": "44c7078d6da2c2a1aac1",
+    "url": "/durov_test/static/js/runtime-main.fcf196c8.js"
+  },
+  {
+    "revision": "25a24979466b973edf9b4b8f2ae77e74",
+    "url": "/durov_test/static/media/dots.25a24979.png"
+  },
+  {
+    "revision": "f88b372846f8ec09c668fe7bc39d4f63",
+    "url": "/durov_test/static/media/monitor.f88b3728.png"
+  },
+  {
+    "revision": "1bdc57c560c8f47bb7d9b6bfd189b6b6",
+    "url": "/durov_test/static/media/waves.1bdc57c5.png"
+  }
+]);
